8caee19d5039391b672edabf10db51da64116a20
epusso <epusso@makingsense.com>
2018-05-19 14:20:53 -0300

    Update names for controller and model:
    - user_controller was renamed as login
    - user_model was renamed as model_user

--- Aventon/application/controllers/login.php
+++ Aventon/application/controllers/login.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class register extends CI_Controller {
+class login extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
@@ -13,31 +13,35 @@ class register extends CI_Controller {
     }
 
     public function index() {
-        $data['title'] = 'Register Aventon';
+        $data['title'] = 'Login Aventon';
         //load view and pass the data
         $this->load->view('templates/header', $data);
-        $this->load->view('view_register');
+        $this->load->view('view_login');
         $this->load->view('templates/footer');
     }
 
     function existEmail() {
         $email = $this->input->post('email');
         //verifies email exists in DB
-        return (!($this->user_model->is_registered($email)));
+        return ($this->model_user->is_registered($email));
+    }
+
+    function validPass() {
+        //verifies password matches with user password
+        $email = $this->input->post('email');
+        $pass = $this->input->post('password');
+        return ($this->model_user->validate_credentials($email, $pass));
     }
 
     public function validar() {
-        // basic required field
-        $this->form_validation->set_rules('name', 'name', 'required|alpha');
-        $this->form_validation->set_rules('surname', 'surname', 'required|alpha');
-        $this->form_validation->set_rules('email', 'email', 'required|valid_email|callback_existEmail');
-        $this->form_validation->set_rules('password', 'password', 'required');
-        $this->form_validation->set_rules('passwordRepeat', 'passwordRepeat', 'required|matches[password]');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|callback_existEmail'); //nombre campo, label men error, validación
+        $this->form_validation->set_rules('password', 'Password', 'required|callback_validPass');
 
         if ($this->form_validation->run() == FALSE) {
+            // no pasa validacion, vuelvo a mostrar el formulario con los datos precargados
             $this->load->view('fail');
         } else {
-            // load success template...
+            // validacion exitosa
             $this->load->view('success');
         }
     }
--- Aventon/application/views/view_login.php
+++ Aventon/application/views/view_login.php
@@ -16,7 +16,7 @@
     <link href="../assets/css/signin.css" rel="stylesheet">
 </head>
 <body class="text-center">
-    <form method="post" action="user_controller/validar" class="form-signin">
+    <form method="post" action="login/validar" class="form-signin">
       <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
