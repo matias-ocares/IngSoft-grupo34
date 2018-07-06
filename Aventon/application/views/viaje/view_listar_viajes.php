8b6188b09693d2d2c441fd8cb3480c0008d1ee28
epusso <epusso@makingsense.com>
2018-07-06 04:04:36 -0300

    aplique css para búsqueda de viajes

--- Aventon/application/views/viaje/view_listar_viajes.php
+++ Aventon/application/views/viaje/view_listar_viajes.php
@@ -3,46 +3,53 @@
 
 <div class="col-sm-8 text-left">
 
-    <form id="formulario2" name="formAuto" method="post" action="<?php echo base_url(); ?>viaje/verificar_search" class="form-signin">
-        <p>Desde</p>
-        <input type="text" id="search_origen" name="search_origen" class="form-control" placeholder="Origen"  required autofocus value="<?php echo $this->session->userdata('origen'); ?>">        
+    <form id="formBuscarViaje" name="formBuscarViaje" method="post" action="<?php echo base_url(); ?>viaje/verificar_search" class="form-signin">
+        <div style="display:inline">
+            <label class="search_viaje_label"> Desde <input type="text" id="search_origen" name="search_origen" class="form-control" placeholder="Origen"  required autofocus value="<?php echo $this->session->userdata('origen'); ?>"> </label>      
 
-        <p> Hasta</p>         
-        <input type="text" id="search_destino" name="search_destino" class="form-control" placeholder="Destino"  required autofocus value="<?php echo $this->session->userdata('destino'); ?>">        
+            <label class="search_viaje_label"> Hasta <input type="text" id="search_destino" name="search_destino" class="form-control" placeholder="Destino"  required autofocus value="<?php echo $this->session->userdata('destino'); ?>"> </label>   
 
-        <p> Fecha</p>
-        <input type="date" id= "search_fecha" name="search_fecha" value="<?php echo $this->session->userdata('fecha'); ?>" class="form-control" placeholder="Fecha: dd/mm/aaaa"  autofocus 
-               min="<?php $hoy = date("Y-m-d");
-echo $hoy; ?>" 
-               max="<?php echo date("Y-m-d", strtotime("+30 days")); ?>">
-        <button class="btn btn-lg btn-primary btn-block btn_perfil" type="submit" onClick="return validacionBusqueda();"> Buscar </button>
+            <label class="search_viaje_label"> Fecha <input type="date" id= "search_fecha" name="search_fecha" value="<?php echo $this->session->userdata('fecha'); ?>" class="form-control" placeholder="Fecha: dd/mm/aaaa"  autofocus 
+                                                            min="<?php
+                                                            $hoy = date("Y-m-d");
+                                                            echo $hoy;
+                                                            ?>" 
+                                                            max="<?php echo date("Y-m-d", strtotime("+30 days")); ?>">
+            </label>
 
-    </form>  
-    <a href="../viaje/mostrar_todos"> Mostrar todos </a>
-
-    <h1 class="h11">Listado de viajes</h1>
-    <div class="container">  
-<?php if ($this->session->flashdata('notifico')): ?>
-
-            <p style="color:green;"><b>  <?php echo $this->session->flashdata('notifico') ?></b></p>
-
-        <?php endif; ?>
-<?php if ($exito): ?>
-
-            <p style="color:green;"><b> <?php echo $exito ?></b> </p>
-
-        <?php endif; ?> 
-<?php if ($error): ?>
-
-            <p style="color:red;"> <b><?php echo $error ?> </b></p>
-
-            <?php endif; ?>
-        <div class="table-responsive">   
-        <?php echo $this->table->generate(); ?>
+            <button class="btn btn-lg btn-primary btn_search_viaje" type="submit" onClick="return validacionBusqueda();"> Buscar </button>
+            <a id="mostrar_todos" href="../viaje/mostrar_todos"> Mostrar todos </a>
         </div>
-<?php echo $this->pagination->create_links(); ?>
+    </form>  
+
+    <?php if ($this->session->flashdata('notifico')): ?>
+
+        <p style="color:red;"> <b> <?php echo $this->session->flashdata('notifico') ?> </b> </p>
+    <?php endif; ?>
+    <?php if ($exito): ?>
+
+        <p style="color:green;"><b> <?php echo $exito ?></b> </p>
+
+    <?php endif; ?> 
+    <?php if ($error): ?>
+
+        <p style="color:red;"> <b><?php echo $error ?> </b></p>
+
+    <?php endif; ?>
+
+
+    <h1 class="h11">Listado de viajes</h1>    
+
+
+    <div class="container">  
+
+        <div class="table-responsive">   
+            <?php echo $this->table->generate(); ?>
+        </div>
+        <?php echo $this->pagination->create_links(); ?>
 
     </div>  
+
 
 </div> 
 
@@ -72,10 +79,10 @@ echo $hoy; ?>"
             alert("[!] El campo destino contiene caracteres no permitidos");
             return false;
         }
-        
-        if ( (origen.trim()).toUpperCase() == (destino.trim()).toUpperCase() ){
+
+        if ((origen.trim()).toUpperCase() == (destino.trim()).toUpperCase()) {
             //remuevo los espacios delante y detrás
-            document.getElementById("search_origen").value =origen.trim();
+            document.getElementById("search_origen").value = origen.trim();
             document.getElementById("search_destino").value = destino.trim();
             alert("[!] Origen y Destino deben ser diferentes");
             return false;
